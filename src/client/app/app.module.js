1393ed8e379a400d6f6764c4d9f4497fcbd8026f
Vu Hoang Viet <vietvuhoang@gmail.com>
2016-10-20 14:53:08 +0700

    Change for Router

--- src/client/app/app.module.js
+++ src/client/app/app.module.js
@@ -7,25 +7,28 @@ angular.module('currencySwapApp', [
     'appHeader'
 ]).run(function ($rootScope, $cookies, $location) {
 
-    $rootScope.loggedIn = false;
+    $rootScope.loggedIn = true;
 
-    var token = $cookies.get(global.tokenKey);
+    var token = $cookies.get(global.TOKEN);
 
     if (!token) {
         $rootScope.loggedIn = false;
         $location.path(routes.LOGIN);
+    } else if ( $location.path() == routes.LOGIN ) {
+        $location.path(routes.HOME);        
     }
+
 }).controller('CurrencySwapController', [
-        '$rootScope',
-        '$scope',
-        function ($rootScope, $scope) {
-            if ($rootScope.loggedIn) {
-                $scope.loggedIn = true;
-            }
+    '$rootScope',
+    '$scope',
+    function ($rootScope, $scope) {
+        if ($rootScope.loggedIn) {
+            $scope.loggedIn = true;
         }
-    ]
-).constant('GLOBAL_CONSTANT', {
-}).constant('ERROR_MSG', {
-    INVALID_USR_OR_PWD_MSG: 'Invalid username or password !',
-    EMPTY_USR_OR_PWD_MSG: 'Empty username or password !'
-});
+    }
+]
+    ).constant('GLOBAL_CONSTANT', {
+    }).constant('ERROR_MSG', {
+        INVALID_USR_OR_PWD_MSG: 'Invalid username or password !',
+        EMPTY_USR_OR_PWD_MSG: 'Empty username or password !'
+    });
