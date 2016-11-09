847d9421b7c1e98114284ebb758ea8e2837ac34f
thelonglqd <thelonglqd@gmail.com>
2016-11-10 02:19:27 +0700

    Enhance users list feature 1. Add pagination to users list feature 2. Add registered date to Member table in database

--- src/server/controllers/orders.js
+++ src/server/controllers/orders.js
@@ -13,12 +13,10 @@ module.exports = function (app) {
     var router = app.loopback.Router();
     
     router.get('/', function (req, res) {
-        console.log('come to get order');
         res.status(200).send({ message : 'pong' });
     });
 
     router.get('/:id', function (req, res) {
-        console.log('come to get order id');
         res.status(200).send({ message : 'pong' });        
     });
 
