e0d19912711a0842b258f7bab61811fee2581467
vietn <vietn@vn.ibm.com>
2016-11-23 16:13:53 +0700

    implement support

--- src/client/app/components/support/support.component.js
+++ src/client/app/components/support/support.component.js
@@ -12,5 +12,16 @@ angular.module('support')
             'NavigationHelper',
             'GLOBAL_CONSTANT',
             function supportController($scope, $rootScope, $timeout, CookieService, SupportService, PermissionService, NavigationHelper, GLOBAL_CONSTANT) {
+                $scope.messageTitle = 'Message to Admin';
+                $scope.support = {title: '', message: ''};
+                $scope.init = function() {
+                    $('[data-toggle="popover"]').popover();
+                };
+                $scope.save = function() {
+                    
+                };
+
+                // init
+                $scope.init();
             }]
     });
