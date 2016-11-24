40cb33ef11282e49fba7567a9d2b0198fcde0120
vietn <vietn@vn.ibm.com>
2016-11-24 10:03:35 +0700

    implement order services

--- src/client/app/components/orders/orders.service.js
+++ src/client/app/components/orders/orders.service.js
@@ -2,22 +2,24 @@
 'use strict';
 
 angular.module('orders').factory('OrdersService', ['ConnectorService', function (ConnectorService, PermissionService) {
-    return $.extend({}, ConnectorService, getSwappingOrders: function (headers) { //status == 2
+    return $.extend({}, ConnectorService, {
+        getSwappingOrders: function() { //status == 2
             return this.get(apiRoutes.API_ORDERS_SWAPPING);
         },
-        getConfirmedOrders: function(headers){ // status = 3 || 4
+        getConfirmedOrders: function(){ // status = 3 || 4
             return this.get(apiRoutes.API_ORDERS_CONFIRMED);
         },
-        getHistoryOrders: function (headers) { // status = 5 || 6 
+        getHistoryOrders: function() { // status = 5 || 6 
             return this.get(apiRoutes.API_ORDERS_HISTORY);
         },
-        getSumittedOrders: function (headers) { // status = 1 
+        getSumittedOrders: function() { // status = 1 
             return this.get(apiRoutes.API_ORDERS_SUBMITTED);
         },
-        getOrderById : function(orderId, headers){
+        getOrderById : function(orderId, ){
             return this.get(apiRoutes.API_ORDERS_ID +  '/' + orderId);
         },
-        getAllOrders: function(headers){
+        getAllOrders: function(){
             return this.get(apiRoutes.API_ORDERS);
         });
+    });
 }]);
