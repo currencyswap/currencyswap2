06eaa844502a6540dcfcfd65bc9e04d4faba4966
vietn <vietn@vn.ibm.com>
2016-11-26 11:10:13 +0700

    Update UI style for order create form, revise/refine code of orderservice and controller

--- src/server/controllers/orders.js
+++ src/server/controllers/orders.js
@@ -10,202 +10,106 @@ var statusTypeService = require('../services/status-type-service');
 var orderConverter = require('../converters/order-converter');
 var appConfig = require('../libs/app-config');
 var util = require('util');
-var gen = require('../libs/utilities/crypto_util'); 
+var cryptoUtil = require('../libs/utilities/crypto_util'); 
 
+var CODE_LENGTH = 10;
 module.exports = function (app) {
     var router = app.loopback.Router();
-    var ownerRelation = {
-		'relation' : 'owner'
-	};
-    var accepterRelation = {
-    		'relation' : 'accepter'
-	};
 
-	var giveCurrencyRelation = {
-		'relation' : 'giveCurrency'
-	};
-	var getCurrencyRelation = {
-		'relation' :'getCurrency'
-	}
-	
-	var statusRelation = {
-		'relation' : 'status'
-	};
-	
+    var generateCode = function(id){
+        var key = id + (new Date()) + Math.random();
+        return cryptoUtil.createHash(key).substring(0, CODE_LENGTH);
+    }
+
+    var updateOrderStatus = function (req, res, statusId){
+        var orderId = req.params.id;
+        service.updateOrderStatus(orderId, statusId).then(function(resp){
+            return res.send(resp);
+          }, function(err){
+              return res.status(500).send(errorUtil.createAppError(errors.SERVER_GET_PROBLEM));
+          });
+    };
+
+    router.get('/genOrderCode', function (req, res) {
+        res.send(generateCode(req.currentUser.id + req.currentUser.username));
+    });
     router.post('/', function (req, res) {
-    	
         // handle register request
         var clientOrderData = req.body;
         var newOrder = orderConverter.convertOrderData(clientOrderData);
         newOrder.ownerId = req.currentUser.id;
-        newOrder.code = gen.createHash('order'+new Date()).substring(0, 10);
-        
-        console.log("save new order : " + JSON.stringify(clientOrderData));
-        
-        statusTypeService.filterStatusType({}).then(function(resp){
-        	for(var i in resp){
-        		if(constant.STATUS_TYPE.SUBMITTED == resp[i].name){
-        			newOrder.statusId = resp[i].id;
-        			break;
-        		}
-        	}
-        	
-        	//return res.status(constant.HTTP_SUCCESS_CODE).send(newOrder);
-        	
-        	service.saveOrder(newOrder).then(function (dataSave) {
-        		return res.status(200).send({dataSave : dataSave, newOrder : newOrder});
-        	},function (err) {
-        		return res.status(500).send(errorUtil.createAppError(errors.SERVER_GET_PROBLEM));
-        	});
-        }, function(err){
-            return res.status(500).send(errorUtil.createAppError(errors.SERVER_GET_PROBLEM));
-        });
-        
+        newOrder.code = clientOrderData.code||generateCode(req.currentUser.id + req.currentUser.username);
+        newOrder.statusId = constant.STATUS_TYPE.SUBMITTED_ID;
+        console.log("saving new order : " + JSON.stringify(clientOrderData));
+
+        service.saveOrder(newOrder).then(function (dataSave) {
+            return res.status(200).send({dataSave : dataSave, newOrder : newOrder});
+            },function (err) {
+                    return res.status(500).send(errorUtil.createAppError(errors.SERVER_GET_PROBLEM));
+            });
     });
-    
+
     router.get('/swapping', function (req, res) {
-    	var user = req.currentUser;
-    	console.log(JSON.stringify(user.id));
-		var filter = {
-			'where': {
-    			and: [{ or: [{'ownerId': user.id}, 
-    			             {'accepterId': user.id}] 
-    					},
-    			      { statusId: 2 }
-    			]
-    		},
-    		'include' : [ ownerRelation, accepterRelation, giveCurrencyRelation, getCurrencyRelation, statusRelation]
-		}
-        service.filterOrders(filter).then(function(resp){
-        	console.log(resp);
+        service.getUserSwappingOrders(req.currentUser.id).then(function(resp){
             return res.send(resp);
         }, function(err){
             return res.status(500).send(errorUtil.createAppError(errors.SERVER_GET_PROBLEM));
         });
     });
-    function updateOrder(orderId, statusId){
-    	console.log(orderId, statusId);
-		var order = {
-			statusId: statusId
-		}
-		var where = {
-			and : [{'id': orderId}]
-		}
-        service.updateOrder(where, order).then(function(resp){
-        	console.log(resp);
-            return res.send(resp);
-          }, function(err){
-        	  console.log(err);
-              return res.status(500).send(errorUtil.createAppError(errors.SERVER_GET_PROBLEM));
-          });	
-    }
-    router.get('/swapping/cancel/:id', function (req, res) {
-    	var orderId = req.params.id;
-    	return updateOrder(orderId, 1);
-    });
-    router.get('/swapping/confirm/:id', function (req, res) {
-    	var orderId = req.params.id;
-    	console.log('swapping to confirm');
-    	return updateOrder(orderId, 3);
-    });
-
     router.get('/confirmed', function (req, res) {
-    	var user = req.currentUser;
-    	console.log(JSON.stringify(user.id));
-		var filter = {
-			where: {
-    			and: [{ or: [{'ownerId': user.id}, 
-    			             {'accepterId': user.id}] 
-    					},
-    			      {or : [ {statusId: 3}, { statusId: 4} ]}
-    			]
-    		},
-    		'include' : [ ownerRelation, accepterRelation, giveCurrencyRelation, getCurrencyRelation, statusRelation]
-		}
-        service.filterOrders(filter).then(function(resp){
-          return res.send(resp);
-
+        service.getUserConfirmedOrders(req.currentUser.id).then(function(resp){
+            return res.send(resp);
         }, function(err){
             return res.status(500).send(errorUtil.createAppError(errors.SERVER_GET_PROBLEM));
-        });        
-    });
-    router.get('/confirmed/cancel/:id', function (req, res) {
-    	var orderId = req.params.id;
-    	return updateOrder(orderId, 1);
-    });
-    router.get('/confirmed/clear/:id', function (req, res) {
-    	var orderId = req.params.id;
-    	return updateOrder(orderId, 5); 	
+        });
     });
     router.get('/submitted', function (req, res) {
-    	var user = req.currentUser;
-		var filter = {
-			where: {
-    			and: [{'ownerId': user.id}, 
-    			      { statusId: 1 }
-    			]
-    		},
-    		'include' : [ ownerRelation, accepterRelation, giveCurrencyRelation, getCurrencyRelation, statusRelation]
-		}
-        service.filterOrders(filter).then(function(resp){
+        service.getUserSubmittedOrders(req.currentUser.id).then(function(resp){
             return res.send(resp);
         }, function(err){
             return res.status(500).send(errorUtil.createAppError(errors.SERVER_GET_PROBLEM));
         });
     });
+    
+    router.get('/swapping/cancel/:id', function (req, res) {
+        return updateOrderStatus(req, res, constant.STATUS_TYPE.SUBMITTED_ID);
+    });
+    router.get('/swapping/confirm/:id', function (req, res) {
+        return updateOrderStatus(req, res, constant.STATUS_TYPE.CONFIRMED_ID);
+    });
+
+    router.get('/confirmed/cancel/:id', function (req, res) {
+        return updateOrderStatus(req, res, constant.STATUS_TYPE.SUBMITTED_ID);
+    });
+    router.get('/confirmed/clear/:id', function (req, res) {
+        return updateOrderStatus(req, res, constant.STATUS_TYPE.CLEARED_ID);
+    });
+
     router.get('/suggest', function (req, res) {
-    	var user = req.currentUser;
-    	console.log('submitted', JSON.stringify(user.id));
-		var filter = {
-			where: {
-    			and: [
-    			      //{'ownerId': {'neq' : user.id} }, 
-    			      { statusId: 1 }
-    			]
-    		},
-    		'include' : [ ownerRelation, accepterRelation, giveCurrencyRelation, getCurrencyRelation, statusRelation]
-		
-		}
-        service.filterOrders(filter).then(function(resp){
+        var give = req.query.give;
+        var get = req.query.get;
+        var rate = req.query.rate;
+        var fixed = req.query.fixed;// fixed = 1 (give) , 2 (get), 3 (rate)
+        service.getSuggestOrders(req.currentUser.id, give, get, rate, fixed).then(function(resp){
             return res.send(resp);
         }, function(err){
             return res.status(500).send(errorUtil.createAppError(errors.SERVER_GET_PROBLEM));
         });
-        
     });
     router.get('/history', function (req, res) {
-    	var user = req.currentUser;
-		var filter = {
-			where: {
-    			and: [{ or: [{'ownerId': user.id}, 
-    			             {'accepterId': user.id}] 
-    					},
-    			      {or : [ {statusId: 5}, { statusId: 6} ]}
-    			]
-    		},
-    		'include' : [ ownerRelation, accepterRelation, giveCurrencyRelation, getCurrencyRelation, statusRelation]
-		}
-        service.filterOrders(filter).then(function(resp){
+        service.getUserHistoryOrders(req.currentUser.id).then(function(resp){
           return res.send(resp);
-
         }, function(err){
             return res.status(500).send(errorUtil.createAppError(errors.SERVER_GET_PROBLEM));
         });
     });
     router.get('/:id', function (req, res) {
-    	var orderId = req.params.id;
-		var filter = {
-			where: {
-    			"id": orderId
-    		},
-    		'include' : [ ownerRelation, accepterRelation, giveCurrencyRelation, getCurrencyRelation, statusRelation]
-		}
-        service.getOrderById(filter).then(function(resp){
+        var orderId = req.params.id;
+        service.getOrderById(orderId).then(function(resp){
           return res.send(resp);
-
         }, function(err){
             return res.status(500).send(errorUtil.createAppError(errors.SERVER_GET_PROBLEM));
-        	
+                
         });
     });
     return router;
