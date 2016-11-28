09156ecef94c686c31bde11edd840d00d51a8b12
Nguyen Thap <nguyen.danh.thap@vsi-international.com>
2016-11-29 00:36:01 +0700

    Create page User Detail

--- src/client/app/components/user-details/user-details.component.js
+++ src/client/app/components/user-details/user-details.component.js
@@ -20,6 +20,18 @@ angular.module('userDetails').component('userDetails', {
             var headers = {};
 
             $scope.user = {};
+            $scope.selectedStatus = {};
+            $scope.isEditting = false;
+            $scope.formats = ['MMM dd, yyyy', 'yyyy/MM/dd', 'dd.MM.yyyy', 'shortDate'];
+            $scope.format = $scope.formats[0];
+            $scope.altInputFormats = ['M!/d!/yyyy'];
+
+            $scope.userStatusesList = {};
+            $scope.userStatusesList.activated = GLOBAL_CONSTANT.ACTIVATED_USER_STATUS;
+            $scope.userStatusesList.blocked = GLOBAL_CONSTANT.BLOCKED_USER_STATUS;
+            $scope.userStatusesList.pending = GLOBAL_CONSTANT.PENDING_USER_STATUS;
+            $scope.userStatusesList.new = GLOBAL_CONSTANT.NEW_USER_STATUS;
+            $scope.userStatusesList.deactivated = GLOBAL_CONSTANT.DEACTIVATED_USER_STATUS;
 
             headers[httpHeader.CONTENT_TYPE] = contentTypes.JSON;
             headers[httpHeader.AUTHORIZARION] = autheticateType.BEARER + token;
@@ -30,6 +42,8 @@ angular.module('userDetails').component('userDetails', {
                     $scope.user = response.data;
                     $scope.user.birthday = new Date( $scope.user.birthday );
                     $scope.user.expiredDate = new Date( $scope.user.expiredDate );
+                    $scope.fullName = $scope.user.fullName;
+                    $scope.selectedStatus.selectedStatus = $scope.user.status;
                 }, function ( err ) {
                     console.error("ERROR : %s", err );
                     $rootScope.error = {
@@ -38,5 +52,15 @@ angular.module('userDetails').component('userDetails', {
                         message: err.message
                     };
                 });
+            $scope.changeStateToEdit = function () {
+                $scope.isEditting = true;
+            };
+            $scope.changeRole = function () {
+                $scope.errorSetOwnRole = false;
+            };
+            $scope.onBackStep = function(){
+                $location.path(routes.USERS);
+                $window.scrollTo(0, 0);
+            }
         }]
 });
