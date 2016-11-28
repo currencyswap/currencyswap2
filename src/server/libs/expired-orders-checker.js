d6507f40a7d7484a072dbbda7b6f86571281fdc3
vietn <vietn@vn.ibm.com>
2016-11-28 12:52:34 +0700

    implement expired orders checker

---
+++ src/server/libs/expired-orders-checker.js
@@ -0,0 +1,31 @@
+'use strict';
+/**
+ * @author Viet Nghiem
+ */
+var CronJob = require('cron').CronJob;
+
+module.exports = function() {
+    var performCheck = function() {
+        console.log('CronJob expired-orders-checker::performCheck is starting...', new Date());
+    };
+    
+    var init = function() {
+        var startTime = new Date();
+        startTime.setSeconds(startTime.getSeconds()+10);
+        var jobOnce = new CronJob(startTime, function() {
+                performCheck();
+                jobOnce.stop();
+          }, function () {
+              jobInterval.start();
+              console.log('CronJob expired-orders-checker:: is completed', new Date());
+          }, true);
+        var jobInterval = new CronJob('* 15 * * * *', function() {
+            performCheck();
+          }, function () {
+              console.log('CronJob expired-orders-checker:: is completed', new Date());
+          }
+        );
+    };
+    
+    init();
+};
