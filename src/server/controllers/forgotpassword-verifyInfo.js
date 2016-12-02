9d5ec55a600050c24258a0aeae990a94b16af6e2
thelonglqd <thelonglqd@gmail.com>
2016-12-02 15:08:54 +0700

    fix issue left email field blank then click submit in forgot password page

--- src/server/controllers/forgotpassword-verifyInfo.js
+++ src/server/controllers/forgotpassword-verifyInfo.js
@@ -2,13 +2,18 @@
 var userService = require('../services/user-service');
 var crypto = require('crypto');
 var constant = require('../libs/constants/constants');
+var errorUtils = require('../libs/errors/error-util');
+var errors = require('../libs/errors/errors');
 
 module.exports = function (app) {
     var router = app.loopback.Router();
 
     router.post('/', function (req, res) {
         var userEmail = req.body.email;
-
+        if (!userEmail || typeof userEmail !== 'string') {
+            return res.status(constant.HTTP_FAILURE_CODE).send(errorUtils.createAppError(errors.INVALID_INPUT_TYPE))
+        }
+        
         var options = {};
         var protocolHostAndPort = req.protocol + '://' + req.get('host');
         options.protocolHostAndPort = protocolHostAndPort;
