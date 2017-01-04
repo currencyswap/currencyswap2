1e4e72e79ed06903cf3a7e3b23c26a0f13bdfb88
thelonglqd <thelonglqd@gmail.com>
2017-01-04 13:34:34 +0700

    db changes in exchange rate table

--- src/bin/data/exchanges.js
+++ src/bin/data/exchanges.js
@@ -5,39 +5,24 @@ module.exports = {
     exchanges: [
         {
             createdDate: new Date(Date.now() + 1000),
-            usDollarBuy: 304,
-            usDollarSell: 305,
-            poundBuy: 373,
-            poundSell: 374,
-            euroBuy: 318,
-            euroSell: 319
+            fromCode: 'USD',
+            toCode: 'NGN',
+            buy: 300,
+            sell: 400
         },
         {
-            createdDate: new Date(Date.now()  + 2000),
-            usDollarBuy: 314,
-            usDollarSell: 315,
-            poundBuy: 383,
-            poundSell: 384,
-            euroBuy: 328,
-            euroSell: 329
+            createdDate: new Date(Date.now() + 5000),
+            fromCode: 'EUR',
+            toCode: 'NGN',
+            buy: 400,
+            sell: 500
         },
         {
-            createdDate: new Date(Date.now()  + 3000),
-            usDollarBuy: 324,
-            usDollarSell: 325,
-            poundBuy: 393,
-            poundSell: 394,
-            euroBuy: 338,
-            euroSell: 339
-        },
-        {
-            createdDate: new Date(Date.now() + 4000),
-            usDollarBuy: 334,
-            usDollarSell: 335,
-            poundBuy: 403,
-            poundSell: 404,
-            euroBuy: 348,
-            euroSell: 349
+            createdDate: new Date(Date.now() + 10000),
+            fromCode: 'GBP',
+            toCode: 'NGN',
+            buy: 500,
+            sell: 600
         }
     ]
 };
